b7f4c8aedd6d61df90231b98e43dbe0afb3c6834
megoan <32939633+megoan@users.noreply.github.com>
2020-11-11 17:14:41 +0200

    update

--- android/src/main/kotlin/com/apptreesoftware/flutterwebview/FlutterWebViewPlugin.kt
+++ android/src/main/kotlin/com/apptreesoftware/flutterwebview/FlutterWebViewPlugin.kt
@@ -7,6 +7,7 @@ import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.WindowManager
 import android.webkit.*
 import io.flutter.app.FlutterActivity
 import io.flutter.plugin.common.MethodCall
