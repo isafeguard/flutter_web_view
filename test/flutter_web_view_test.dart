683110f6591cb61e3eaa097166940af81bfba901
yairsafe <31651758+yairsafe@users.noreply.github.com>
2020-11-11 15:13:31 +0200

    android

--- test/flutter_web_view_test.dart
+++ test/flutter_web_view_test.dart
@@ -1,23 +1,23 @@
-import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_web_view/flutter_web_view.dart';
+// import 'package:flutter/services.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:flutter_web_view/flutter_web_view.dart';
 
-void main() {
-  const MethodChannel channel = MethodChannel('flutter_web_view');
+// void main() {
+//   const MethodChannel channel = MethodChannel('flutter_web_view');
 
-  TestWidgetsFlutterBinding.ensureInitialized();
+//   TestWidgetsFlutterBinding.ensureInitialized();
 
-  setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
-  });
+//   setUp(() {
+//     channel.setMockMethodCallHandler((MethodCall methodCall) async {
+//       return '42';
+//     });
+//   });
 
-  tearDown(() {
-    channel.setMockMethodCallHandler(null);
-  });
+//   tearDown(() {
+//     channel.setMockMethodCallHandler(null);
+//   });
 
-  test('getPlatformVersion', () async {
-    expect(await FlutterWebView.platformVersion, '42');
-  });
-}
+//   test('getPlatformVersion', () async {
+//     expect(await FlutterWebView.platformVersion, '42');
+//   });
+// }
